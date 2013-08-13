0bbe22492ce179727db7f4b1253af5c64f980258
Christian Smith <smith@anvil.io>
2013-08-13 08:44:58 -0700

    access token validation test stubs

--- test/authorization/access/ValidateTokenSpec.js
+++ test/authorization/access/ValidateTokenSpec.js
@@ -1,10 +1,63 @@
-describe('bearer token validation', function () {
+describe('access token validation', function () {
 
-  describe('/GET /token', function () {
-    it('should require client/resource server authentication');
-    it('should verify the token existence');
-    it('should verify the token expiration')
-    it('should verify the token scope');
+  describe('GET /token/verify', function () {
+
+    it('should require SSL');
+
+    describe('with valid request', function () {
+      it('should respond 200');
+      it('should respond with JSON');
+      it('should respond with ???');
+    });
+
+    describe('with unauthenticated client', function () {
+      it('should respond 400');
+      it('should respond with JSON');
+      it('should respond with an "invalid_request" error');
+      it('should respond with an error description');
+      it('should respond with an error uri');
+    });
+
+    describe('with more than one authentication method', function () {
+      it('should respond 400');
+      it('should respond with JSON');
+      it('should respond with an "invalid_request" error');
+      it('should respond with an error description');
+      it('should respond with an error uri');
+    });
+
+    describe('with client mismatch', function () {
+      it('should respond 400');
+      it('should respond with JSON');
+      it('should respond with an "invalid_token" error');
+      it('should respond with an error description');
+      it('should respond with an error uri');
+    });
+
+    describe('with unknown access token', function () {
+      it('should respond 400');
+      it('should respond with JSON');
+      it('should respond with an "invalid_token" error');
+      it('should respond with an error description');
+      it('should respond with an error uri');
+    });
+
+    describe('with expired access token', function () {
+      it('should respond 400');
+      it('should respond with JSON');
+      it('should respond with an "invalid_token" error');
+      it('should respond with an error description');
+      it('should respond with an error uri');
+    });
+
+    describe('with insufficient scope', function () {
+      it('should respond 400');
+      it('should respond with JSON');
+      it('should respond with an "insufficient_scope" error');
+      it('should respond with an error description');
+      it('should respond with an error uri');
+    });
+
   });
 
 });
