f089e8a69804b3ff8727fe77514d9c7d87c0987e
Miguel Cobá <miguel.coba@gmail.com>
2013-10-03 23:35:32 -0500

    Filters out private attributes when updating a User
    Fixes https://github.com/christiansmith/OAuth2Server/issues/4

--- test/routes/rest/UsersRoutesSpec.js
+++ test/routes/rest/UsersRoutesSpec.js
@@ -99,6 +99,15 @@ describe('User REST Routes', function () {
         res.body[0].email.should.equal(validUser.email);
       });
 
+      // should not leak private attributes
+      it('should not include hash in response', function () {
+        expect(res.body[0].hash).equals(undefined);
+      });
+
+      it('should not include salt in response', function () {
+        expect(res.body[0].salt).equals(undefined);
+      });
+
     });
 
   });
@@ -152,6 +161,15 @@ describe('User REST Routes', function () {
 
       it('should respond with a user', function () {
         res.body.email.should.equal(validUser.email);
+      });
+
+      // should not leak private attributes
+      it('should not include hash in response', function () {
+        expect(res.body.hash).equals(undefined);
+      });
+
+      it('should not include salt in response', function () {
+        expect(res.body.salt).equals(undefined);
       });
 
     });
@@ -217,8 +235,13 @@ describe('User REST Routes', function () {
         User.backend.documents[0].email.should.equal(validUser.email);
       });
 
+      // should not leak private attributes
       it('should not include hash in response', function () {
         expect(res.body.hash).equals(undefined);
+      });
+
+      it('should not include salt in response', function () {
+        expect(res.body.salt).equals(undefined);
       });
 
     });
@@ -230,7 +253,7 @@ describe('User REST Routes', function () {
         request(app)
           .post('/v1/users')
           .set('Authorization', 'Basic ' + new Buffer(credentials.key + ':' + credentials.secret).toString('base64'))
-          .send({ password: 'secret' })
+          .send({ password: 'secret' }) // doesn't have required email attribute
           .end(function (error, response) {
             err = error;
             res = response;
@@ -248,6 +271,10 @@ describe('User REST Routes', function () {
 
       it('should respond with validation errors', function () {
         res.body.errors.should.be.defined;
+      });
+
+      it('should respond with a email required error', function () {
+        expect(res.body).to.have.deep.property('errors.email.attribute', 'required');
       });
 
     });
@@ -322,7 +349,65 @@ describe('User REST Routes', function () {
         User.backend.documents[0].hash.should.not.equal(hash);
       });
 
+      it('should not include hash in response', function () {
+        expect(res.body.hash).equals(undefined);
+      });
+
+      it('should not include salt in response', function () {
+        expect(res.body.salt).equals(undefined);
+      });
+
     });
+
+    describe('with valid request but password not updated', function () {
+
+      var salt, hash;
+
+      before(function (done) {
+        User.backend.reset();
+        User.create(validUser, function (err, instance) {
+          salt = User.backend.documents[0].salt;
+          hash = User.backend.documents[0].hash;
+          request(app)
+            .put('/v1/users/' + instance._id)
+            .set('Authorization', 'Basic ' + new Buffer(credentials.key + ':' + credentials.secret).toString('base64'))
+            .send({ email: 'updated@example.com' })
+            .end(function (error, response) {
+              err = error;
+              res = response;
+              done();
+            });
+        });
+      });
+
+      it('should respond 200', function () {
+        res.statusCode.should.equal(200);
+      });
+
+      it('should respond with JSON', function () {
+        res.headers['content-type'].should.contain('application/json');
+      });
+
+      it('should update a user', function () {
+        User.backend.documents[0].email.should.equal('updated@example.com');
+      });
+
+      it('should not alter tha hash and salt', function () {
+        User.backend.documents[0].salt.should.equal(salt);
+        User.backend.documents[0].hash.should.equal(hash);
+      });
+
+      it('should not include hash in response', function () {
+        expect(res.body.hash).equals(undefined);
+      });
+
+      it('should not include salt in response', function () {
+        expect(res.body.salt).equals(undefined);
+      });
+
+    });
+
+
 
     describe('with invalid request', function () {
 
